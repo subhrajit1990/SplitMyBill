f723ba9f07115397554910c8e9d6cd62f6c68e1e
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-08-15 11:51:56 +0530

    Update MiscellaneousController.java

--- src/main/java/bill/manager/controller/MiscellaneousController.java
+++ src/main/java/bill/manager/controller/MiscellaneousController.java
@@ -5,14 +5,10 @@ package bill.manager.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,12 +16,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import bill.manager.model.FetchCardsRequestWrapper;
-import bill.manager.model.GameCardsListResponse;
-import bill.manager.model.GameCardsResponseWrapper;
-import bill.manager.model.ResponseHeader;
+
 import bill.manager.service.MiscellaneousService;
-import bill.manager.utils.CommonUtils;
+
 
 /**
  * @author Troublem@ker
@@ -60,42 +53,6 @@ public class MiscellaneousController {
 
 	}
 
-	@ApiResponses({ @ApiResponse(code = 200, message = "Get Cards API is reachable"),
-			@ApiResponse(code = 408, message = "Service Timed Out"),
-			@ApiResponse(code = 500, message = "Internal Server Error"),
-			@ApiResponse(code = 404, message = "Get Cards API is not reachable") })
-	@ApiOperation(value = "Get Cards", notes = "Get Cards")
-	@PostMapping(value = "/fetchCards",  consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<GameCardsResponseWrapper> getCards(
-			
-			@RequestBody FetchCardsRequestWrapper fetchCardsrequestWrapper,
-			@RequestHeader("masterTxnRefNo") String masterTxnRefNo,
-			@RequestHeader("channel") String channel) {
-		logger.info("Started the execution for the get cards request with masterTxnRefNo :: " + masterTxnRefNo);
-		HttpStatus httpStatus = null;
-		httpStatus = HttpStatus.OK;
-		ResponseHeader responseHeader = new ResponseHeader();
-		GameCardsResponseWrapper gameCardsResponseWrapper = new GameCardsResponseWrapper();
-		GameCardsListResponse gameCardsResponse = new GameCardsListResponse();
-		try {
-			gameCardsResponse = miscellaneousService.fetchCards(channel, masterTxnRefNo);
-			if (gameCardsResponse.getGameCardResponse().isEmpty()) {
-
-				CommonUtils.generateHeaderForNoResult(responseHeader);
-
-			} else {
-				CommonUtils.generateHeaderForSuccess(responseHeader);
-				gameCardsResponseWrapper.setGameCardListResponse(gameCardsResponse);
-			}
-
-		} catch (Exception e) {
-			logger.error("exception happened during get cards service execution :: " + e.getStackTrace());
-			CommonUtils.generateHeaderForGenericError(responseHeader);
-		}
-		gameCardsResponseWrapper.setResponseHeader(responseHeader);
-		logger.info("Finished the execution for the get cards request with masterTxnRefNo :: " + masterTxnRefNo);
-		return new ResponseEntity<>(gameCardsResponseWrapper, httpStatus);
-
-	}
+	
 
 }
