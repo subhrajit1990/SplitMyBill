15454ec1c38d445a51b3b0668b2e6283a18d30b5
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-12-26 15:12:24 +0530

    Update SecurityConfig.java

--- src/main/java/bill/manager/splitmybill/SecurityConfig.java
+++ src/main/java/bill/manager/splitmybill/SecurityConfig.java
@@ -6,14 +6,11 @@ package bill.manager.splitmybill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 /**
  * @author Troublem@ker
@@ -40,12 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
          .antMatchers("/**")
          .hasRole("USER")
          .and()
-         .httpBasic()
-         .and()
-         .cors().configurationSource(configurationSource()).and()
-         .requiresChannel()
-         .anyRequest()
-         .requiresSecure();
+         .httpBasic();
     }
   
     @Autowired
@@ -58,15 +50,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .roles("USER");
     }
     
-    private CorsConfigurationSource configurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.setAllowCredentials(true);
-        config.addAllowedHeader("X-Requested-With");
-        config.addAllowedHeader("Content-Type");
-        config.addAllowedMethod(HttpMethod.POST);
-        source.registerCorsConfiguration("/**", config);
-        return source;
-      }
 }
