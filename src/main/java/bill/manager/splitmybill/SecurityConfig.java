25a7485c9c218ecbce06b21f5a4a19e3505773c0
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-12-26 13:14:44 +0530

    Add files via upload
    security basic auth config

---
+++ src/main/java/bill/manager/splitmybill/SecurityConfig.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package bill.manager.splitmybill;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Troublem@ker
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Value("${spring.security.user.name}")
+	private  String uid;
+	
+	@Value("${spring.security.user.password}")
+	private  String passwd;
+	
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception 
+    {
+        http
+         .csrf().disable()
+         .authorizeRequests().anyRequest().authenticated()
+         .and()
+         .httpBasic();
+    }
+  
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception 
+    {
+        auth.inMemoryAuthentication()
+            .withUser(uid)
+            .password("{noop}"+passwd)
+            .roles("USER");
+    }
+}
