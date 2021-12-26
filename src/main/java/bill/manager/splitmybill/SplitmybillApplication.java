e858b79a7c800257ac3b9bd17cc59754b8980ba4
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-12-26 14:03:37 +0530

    Update SplitmybillApplication.java

--- src/main/java/bill/manager/splitmybill/SplitmybillApplication.java
+++ src/main/java/bill/manager/splitmybill/SplitmybillApplication.java
@@ -3,8 +3,11 @@ package bill.manager.splitmybill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -22,5 +25,16 @@ public class SplitmybillApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SplitmybillApplication.class, args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("").allowedOrigins("https://sharemybill.herokuapp.com/");
+			}
+		};
+	}
+
 
 }
