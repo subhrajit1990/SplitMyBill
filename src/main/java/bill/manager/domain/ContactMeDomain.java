c35a87195d43c84b3873a32fbf72b54894a4fc82
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2022-04-09 11:52:06 +0530

    Update ContactMeDomain.java

--- src/main/java/bill/manager/domain/ContactMeDomain.java
+++ src/main/java/bill/manager/domain/ContactMeDomain.java
@@ -1,31 +1,50 @@
-/**
- * 
- */
 package bill.manager.domain;
 
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * @author Troublem@ker
  */
-public class BillsIdDomain implements Serializable{
+
+@Entity
+@Table(name = "CONTACT_ME")
+public class ContactMeDomain extends CommonEntity implements Serializable{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@Column(name = "group_id",updatable=false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Id
-	@Column(name="bill_id",updatable=false)
-	private String billId;
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "channel")
+	private String channel;
+	
+	@Column(name = "email")
+	private String email;
 
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+	@Column(name = "message")
+	private String message;
+	
+	@Column(name="master_txn_no")
+	private String masterTxnNo;
+
+	
 	public Long getId() {
 		return id;
 	}
@@ -34,46 +53,55 @@ public class BillsIdDomain implements Serializable{
 		this.id = id;
 	}
 
-	public String getBillId() {
-		return billId;
+	public String getName() {
+		return name;
 	}
 
-	public void setBillId(String billId) {
-		this.billId = billId;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((billId == null) ? 0 : billId.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public String getChannel() {
+		return channel;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		BillsIdDomain other = (BillsIdDomain) obj;
-		if (billId == null) {
-			if (other.billId != null)
-				return false;
-		} else if (!billId.equals(other.billId))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+	public void setChannel(String channel) {
+		this.channel = channel;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMasterTxnNo() {
+		return masterTxnNo;
+	}
+
+	public void setMasterTxnNo(String masterTxnNo) {
+		this.masterTxnNo = masterTxnNo;
+	}
 	
 	
-
+	
+	
 }
